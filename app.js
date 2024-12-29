cfdd065bbf6567041575ca8a25b0b74591b5082f
buildingsoftly <a.c.mcmurry@proton.me>
2024-12-29 11:06:22 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function onload() {
+    let whoIndex = Math.floor(Math.random() * who.length);
+    let actionIndex = Math.floor(Math.random() * action.length);
+    let whatIndex = Math.floor(Math.random() * what.length);
+    let whenIndex = Math.floor(Math.random() * when.length);
+
+    let excuse = who[whoIndex] + ' ' + action[actionIndex] + ' ' + what[whatIndex] + ' ' + when[whenIndex];
+    document.querySelector('#excuse').innerHTML = excuse;
+    console.log(excuse);
+    
+}
+onload();
